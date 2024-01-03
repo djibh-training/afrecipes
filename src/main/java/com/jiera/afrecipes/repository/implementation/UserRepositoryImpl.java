e6bb1efd78676142c3949a4109a71cab838f9c90
Djibh <julien.riera@protonmail.com>
2024-01-03 20:30:26 +0100

    WIP(USER) > Add verification url in user create method

--- src/main/java/com/jiera/afrecipes/repository/implementation/UserRepositoryImpl.java
+++ src/main/java/com/jiera/afrecipes/repository/implementation/UserRepositoryImpl.java
@@ -2,17 +2,22 @@ package com.jiera.afrecipes.repository.implementation;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jiera.afrecipes.domain.Role;
 import com.jiera.afrecipes.domain.User;
 import com.jiera.afrecipes.enumeration.RoleType;
+import com.jiera.afrecipes.enumeration.VerificationType;
 import com.jiera.afrecipes.exception.ApiException;
 import com.jiera.afrecipes.repository.RoleRepository;
 import com.jiera.afrecipes.repository.UserRepository;
@@ -28,31 +33,39 @@ public class UserRepositoryImpl implements UserRepository<User> {
 
     private final NamedParameterJdbcTemplate jdbc;
     private final RoleRepository<Role> roleRepository;
+    private final BCryptPasswordEncoder encoder;
 
     @Override
     public User create(User user) {
         // check the email is unique
         if (getEmailCount(user.getEmail().trim().toLowerCase()) > 0)
             throw new ApiException("Email déjà enregistré.");
-        // save new user
         try {
+            // Insert user in database
             KeyHolder holder = new GeneratedKeyHolder();
             SqlParameterSource parameters = getSqlParameterSource(user);
             jdbc.update(UserQuery.INSERT_USER_QUERY, parameters, holder);
             user.setId(holder.getKey().longValue());
-            // add role to user
+            // Add role to user
             roleRepository.addRoleToUser(user.getId(), RoleType.ROLE_USER.name());
             // send verification url
-            // save url in verification table
+            String verificationUrl = getVerificationUrl(UUID.randomUUID().toString(),
+                    VerificationType.ACCOUNT.getType());
+            // Add verification url to new user record
+            jdbc.update(UserQuery.INSERT_ACCOUNT_VERIFICATION_URL_QUERY,
+                    Map.of("userId", user.getId(), "url", verificationUrl));
+
             // send email to user with verification url
-            // return newly created user
-            // if any errors, throw exception with proper message
+            // emailService.sendVerificationUrl(user.getFirstName(), user.getEmail(),
+            // verificationUrl, ACCOUNT);
+            user.setEnabled(false);
+            user.setIsNotLocked(true);
+            return user;
         } catch (EmptyResultDataAccessException exception) {
-
+            throw new ApiException("Aucun rôle trouvé pour ce nom : " + RoleType.ROLE_USER.name());
         } catch (Exception exception) {
+            throw new ApiException("An error occurred. Please try again.");
         }
-
-        return null;
     }
 
     @Override
@@ -84,7 +97,17 @@ public class UserRepositoryImpl implements UserRepository<User> {
     }
 
     private SqlParameterSource getSqlParameterSource(User user) {
-        return null;
+        return new MapSqlParameterSource()
+                .addValue("firstName", user.getFirstName())
+                .addValue("lastName", user.getLastName())
+                .addValue("email", user.getEmail())
+                .addValue("password", encoder.encode(user.getPassword()));
+    }
+
+    private String getVerificationUrl(String key, String type) {
+        // backend url for now to allow testing with Postman
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/verify" + type + "/" + key)
+                .toUriString();
     }
 
 }
