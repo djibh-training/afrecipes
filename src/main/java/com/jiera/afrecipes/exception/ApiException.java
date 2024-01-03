90a6288a8b0f8c360e4825eee85559678fcda062
Djibh <julien.riera@protonmail.com>
2024-01-03 11:37:30 +0100

    WIP(USER) > Add save user with id in database functionality

---
+++ src/main/java/com/jiera/afrecipes/exception/ApiException.java
@@ -0,0 +1,8 @@
+package com.jiera.afrecipes.exception;
+
+public class ApiException extends RuntimeException {
+    public ApiException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/com/jiera/afrecipes/repository/implementation/UserRepositoryImpl.java
+++ src/main/java/com/jiera/afrecipes/repository/implementation/UserRepositoryImpl.java
@@ -1,8 +1,84 @@
 package com.jiera.afrecipes.repository.implementation;
 
-import com.jiera.afrecipes.domain.User;
-import com.jiera.afrecipes.repository.UserRepository;
+import java.util.Collection;
+import java.util.Map;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import com.jiera.afrecipes.domain.User;
+import com.jiera.afrecipes.exception.ApiException;
+import com.jiera.afrecipes.repository.UserRepository;
+import com.jiera.afrecipes.query.UserQuery;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
 public class UserRepositoryImpl implements UserRepository<User> {
+
+    private final NamedParameterJdbcTemplate jdbc;
+
+    @Override
+    public User create(User user) {
+        // check the email is unique
+        if (getEmailCount(user.getEmail().trim().toLowerCase()) > 0)
+            throw new ApiException("Email déjà enregistré.");
+        // save new user
+        try {
+            KeyHolder holder = new GeneratedKeyHolder();
+            SqlParameterSource parameters = getSqlParameterSource(user);
+            jdbc.update(UserQuery.INSERT_USER_QUERY, parameters, holder);
+            user.setId(holder.getKey().longValue());
+        } catch (EmptyResultDataAccessException exception) {
+
+        } catch (Exception exception) {
+        }
+        // add role to user
+        // send verification url
+        // save url in verification table
+        // send email to user with verification url
+        // return newly created user
+        // if any errors, throw exception with proper message
+        return null;
+    }
+
+    @Override
+    public Collection<User> list(int page, int pageSize) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'list'");
+    }
+
+    @Override
+    public User get(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public User update(User data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    private int getEmailCount(String email) {
+        return jdbc.queryForObject(UserQuery.COUNT_USER_EMAIL_QUERY, Map.of("email", email), Integer.class);
+    }
+
+    private SqlParameterSource getSqlParameterSource(User user) {
+        return null;
+    }
 
 }
