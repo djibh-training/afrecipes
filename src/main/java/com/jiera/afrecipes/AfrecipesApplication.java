8c52ca8f7a7834e4bf8df6b8b214467313973d22
Djibh <julien.riera@protonmail.com>
2024-01-03 10:23:51 +0100

    first commit with database schema

---
+++ src/main/java/com/jiera/afrecipes/AfrecipesApplication.java
@@ -0,0 +1,13 @@
+package com.jiera.afrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfrecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfrecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiera/afrecipes/AfrecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiera.afrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfrecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
