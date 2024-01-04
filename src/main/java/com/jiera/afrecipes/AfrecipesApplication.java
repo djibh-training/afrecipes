9b9b27c45222664966049dcbcd8cadd9a484563d
Djibh <julien.riera@protonmail.com>
2024-01-04 14:11:29 +0100

    feat(USER) > Create bean for password encoder

--- src/main/java/com/jiera/afrecipes/AfrecipesApplication.java
+++ src/main/java/com/jiera/afrecipes/AfrecipesApplication.java
@@ -2,12 +2,20 @@ package com.jiera.afrecipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AfrecipesApplication {
+	private static final int STRENGTH = 12;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AfrecipesApplication.class, args);
 	}
 
+	@Bean
+	BCryptPasswordEncoder passwordEncoder() {
+
+		return new BCryptPasswordEncoder(STRENGTH);
+	}
 }
